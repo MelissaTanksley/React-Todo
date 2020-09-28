2ad6540a447aa58e3d2ab35273f7c6496521a73b
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-28 10:23:02 -0700

    initial push and set-up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+t';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: []
+    }
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>I'm so stressed!</h2>
+        <TodoList />
+        <TodoForm />
       </div>
     );
   }
-}
 
 export default App;
